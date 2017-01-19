4300134fb92cc1c0b875efd7702cee0d4c944b6f
Yash Bhargava <yash@algol.ac.iucaa.in>
2017-01-19 14:12:19 +0530

    file for selecting required data

---
+++ saving_important_columns.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+yang_data = np.loadtxt("yangDR7_centrals.txt",skiprows=1,usecols=[1],unpack=True)
+
+X,Y,Z,M200b,PID  = np.loadtxt("out_1.parents",skiprows=16,usecols=[8,9,10,20,41],unpack=True)
+#out_data = np.loadtxt("out_1.parents",skiprows=16,unpack=True)
+#print np.shape(yang_data),np.shape(out_data)
+np.savetxt('abs_mag_data.txt',yang_data,fmt=['%.6f'])
+np.savetxt('x_y_z_m_sim.txt',np.transpose([X,Y,Z,M200b,PID]),fmt=['%.6f','%.6f','%.6f','%.6f','%.6f'], header = 'X\tY\tZ\M200b\tPID')
