3acd31d45a1f68f2d4110a06e353584b129d4301
Yash Bhargava <yash.bhargava1992@gmail.com>
2017-01-19 14:54:20 +0530

    updating the code

--- saving_important_columns.py
+++ saving_important_columns.py
@@ -6,4 +6,4 @@ X,Y,Z,M200b,PID  = np.loadtxt("out_1.parents",skiprows=16,usecols=[8,9,10,20,41]
 #out_data = np.loadtxt("out_1.parents",skiprows=16,unpack=True)
 #print np.shape(yang_data),np.shape(out_data)
 np.savetxt('abs_mag_data.txt',yang_data,fmt=['%.6f'])
-np.savetxt('x_y_z_m_sim.txt',np.transpose([X,Y,Z,M200b,PID]),fmt=['%.6f','%.6f','%.6f','%.6f','%.6f'], header = 'X\tY\tZ\M200b\tPID')
+np.savetxt('x_y_z_m_sim.txt',np.transpose([X,Y,Z,M200b,PID]),fmt=['%.6f','%.6f','%.6f','%.6f','%.6f'], header = 'X\tY\tZ\tM200b\tPID')
