479056e9c0ccb62902a302743745c1b2b3abe406
Yash Bhargava <yash.bhargava1992@gmail.com>
2017-01-19 16:58:54 +0530

    Minor_changes

--- extracting_required_data.py
+++ extracting_required_data.py
@@ -4,6 +4,10 @@ X,Y,Z,M,PID = np.loadtxt('x_y_z_m_sim.txt',unpack=True,skiprows=1)
 
 sel_ind = np.where(PID==-1)[0] 		# Creating a mask for data selection
 
+
+#Filtering the data according to mask
 X,Y,Z,M = X[sel_ind],Y[sel_ind],Z[sel_ind],M[sel_ind]
 
-np.savetxt('x_y_z_m_sim_selected.txt',np.transpose([X,Y,Z,M]),fmt=['%.6f','%.6f','%.6f','%.5e'], header='X\tY\tZ\tM200b')
+## Command to save file
+np.savetxt('x_y_z_m_sim_selected.txt',np.transpose([X,Y,Z,M]),fmt=['%.6f','%.6f','%.6f','%.5e'], header='X\tY\tZ\tM200b') 
+print "Shortened file created"
