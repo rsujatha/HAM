b4115316e4bb10fe26a2a943be4f6758d491ed2b
Yash Bhargava <yash.bhargava1992@gmail.com>
2017-01-19 16:50:23 +0530

    minor changes to code to reduce the size of file saved and second code to filter using PID

---
+++ extracting_required_data.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+X,Y,Z,M,PID = np.loadtxt('x_y_z_m_sim.txt',unpack=True,skiprows=1)
+
+sel_ind = np.where(PID==-1)[0] 		# Creating a mask for data selection
+
+X,Y,Z,M = X[sel_ind],Y[sel_ind],Z[sel_ind],M[sel_ind]
+
+np.savetxt('x_y_z_m_sim_selected.txt',np.transpose([X,Y,Z,M]),fmt=['%.6f','%.6f','%.6f','%.5e'], header='X\tY\tZ\tM200b')
