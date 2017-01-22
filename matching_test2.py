358d0b95cb0d61f179d81a964d8a43c0d9d97326
Sujatha <sujatha.ramakrishnan.7@gmail.com>
2017-01-22 09:46:04 +0530

    Changes to test-2 using absoulte magnitude instead of color

--- matching_test2.py
+++ matching_test2.py
@@ -8,31 +8,41 @@ start_time = time.time()
 	
 # loading text
 luminosity = np.loadtxt ('abs_mag_data.txt')
+luminosity=abs(luminosity)
+print luminosity
 X,Y,Z,M = np.loadtxt ('x_y_z_m_sim_selected.txt',unpack=True, skiprows=1)
 
 #sorting in increasing order
 luminosity.sort()
 M = np.sort(M)
-print M[-11:-1]
-#M.pop()
-M = M[:-1]
+print luminosity
+
 # cdf for both luminosity and mass
-#np.concatenate([luminosity,luminosity[[-1]]])
-#np.concatenate([M,M[[-1]]])
+np.concatenate([luminosity,luminosity[[-1]]])
+np.concatenate([M,M[[-1]]])
 
 luminosityFunction=np.arange(luminosity.size)/float(luminosity.size)
 MFunction=np.arange(M.size)/float(M.size)
 plt.plot(M,MFunction)
+plt.title('Halo Mass Function cdf')
+plt.xlabel('Halo Mass')
+plt.show()
+plt.clf()
+plt.plot(luminosity,luminosityFunction)
+plt.xlabel('Absolute of Absolute Magnitude')
+plt.title('Luminosity Function cdf')
 plt.show()
 plt.clf()
 # interpolating 
-cdf=np.linspace(0,1,1000)
-Luminosity_match=np.interp(cdf, luminosityFunction, luminosity)
-Mass_match=np.interp(cdf,MFunction, M)
+Luminosity_match=np.linspace(luminosity[0],luminosity[-1],100)
+cdf_match=np.interp(Luminosity_match,luminosity,luminosityFunction)
+Mass_match=np.interp(cdf_match,MFunction, M)
 
 #Mass_match
 plt.plot(Mass_match,Luminosity_match,'.r')
 plt.xscale('log')
+plt.ylabel('Absolute of Absolute Magnitue')
+plt.xlabel('Halo Mass')
 plt.show()
 
 
--- saving_important_columns.py
+++ saving_important_columns.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-yang_data = np.loadtxt("yangDR7_centrals.txt",skiprows=1,usecols=[1],unpack=True)
+yang_data = np.loadtxt("yangDR7_centrals.txt",skiprows=1,usecols=[0],unpack=True)
 
 X,Y,Z,M200b,PID  = np.loadtxt("out_1.parents",skiprows=16,usecols=[8,9,10,20,41],unpack=True)
 #out_data = np.loadtxt("out_1.parents",skiprows=16,unpack=True)
