258e718ea242eadb3fb70a556d1e4e582a9a2cee
Sujatha <sujatha.ramakrishnan.7@gmail.com>
2017-01-24 01:20:46 +0530

    Matched with Luminosity

--- data_visualisation.py
+++ data_visualisation.py
@@ -1,15 +1,30 @@
+##plot to visualise the galaxies in the dark matter potential
+##Blue is the dark matter while yello are the galaxies 
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-X,Y,Z,M = np.loadtxt ('x_y_z_m_sim_selected.txt',unpack=True, skiprows=1)
+M,L,X,Y,Z = np.loadtxt ('output_of_3_1.txt',unpack=True, skiprows=1)
+#L=np.sort(L)
+#print L[0],L[-1]
 fig=plt.figure()
 ax = fig.add_subplot(1,1,1, projection='3d')
-ax.plot(X,Y,Z,'.',markersize='.02')
-ax.view_init(elev=30., azim=58)
-ax.set_xlabel('$U_{3n-2}$')
-ax.set_ylabel('$U_{3n-1}$')
-ax.set_zlabel('$U_{3n}$')
-ax.set_title('3-D distribution PiE',rotation='vertical',x=1,y=1)
+s = [float(n)/len(X) for n in range(len(X))]
+ax.scatter(X,Y,Z,'.',color='b',s=0.03)
+ax.scatter(X,Y,Z,'.',color='y',s=L/2e13)		#,markersize='.02'
+ax.set_axis_bgcolor('black')
+#ax.view_init(elev=30., azim=58)
+ax.w_xaxis.set_pane_color((0,0,0))
+ax.w_yaxis.set_pane_color((0,0,0))
+ax.w_zaxis.set_pane_color((0,0,0))
+ax.tick_params(axis='x',colors='white')
+ax.tick_params(axis='y',colors='white')
+ax.tick_params(axis='z',colors='white')
+ax.set_title('Galaxies in Dark Matter',rotation='vertical',x=1,y=1)
+for angle in range(0, 360):
+    ax.view_init(30, angle)
+    plt.draw()
+    plt.pause(.001)
 plt.show()
