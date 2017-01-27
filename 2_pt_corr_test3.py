5e95c492368ad67d94c4f87dfa6a4511e1321ecd
Yash <yash.bhargava1992@gmail.com>
2017-01-27 20:07:52 +0530

    parallelising_the_code

--- 2_pt_corr_test3.py
+++ 2_pt_corr_test3.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pair_counting_function as pcf
 import time
+from joblib import Parallel, delayed
+import multiprocessing as mpg
+
 
 
 start_time = time.time()
@@ -15,12 +18,17 @@ r 		= r_all[1]
 dr 		= 0.1
 DD 		= 0
 skip	= 1000				# Number of iteration to skip before printing timing info
-for n in range (X.size):			#Put X.size later
-	DD += pcf.counter_version2(r,dr,X,Y,Z,n)
-	if n%skip==0:
-		print "Running loops {} iterations done,\nTime for prev {} run is\t:".format(n,skip),time.time()-start_time
-		start_time = time.time()
-
+#~ for n in range (X.size):			#Put X.size later
+	#~ DD += pcf.counter_version2(r,dr,X,Y,Z,n)
+	#~ if n%skip==0:
+		#~ print "Running loops {} iterations done,\nTime for prev {} run is\t:".format(n,skip),time.time()-start_time
+		#~ start_time = time.time()
+		
+num_cores = mpg.cpu_count()
+res = Parallel(n_jobs=num_cores)(delayed(pcf.counter_version2)(r,dr,X,Y,Z,n) for n in range (X.size))
+print res 
+DD = np.sum(res)
+print DD
 print "Completed\nTime for entire operation is\t:",time.time()-init_time
 print DD
 
