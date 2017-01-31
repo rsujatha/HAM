8b44bc3d1e691d604720341c560af86e9455ef01
Sujatha Ramakrishnan <sujatha.ramakrishnan.7@gmail.com>
2017-01-31 22:59:42 +0530

    Chnages

--- density_range_sorting.py
+++ density_range_sorting.py
@@ -4,27 +4,31 @@ import pylab
 
 
 
-X,Y,Z,M200b,PID  = np.loadtxt("out_1.parents",skiprows=16,usecols=[8,9,10,20,41],unpack=True)
-np.savetxt('x_y_z_m_sim.txt',np.transpose([X,Y,Z,M200b,PID]),fmt=['%.6f','%.6f','%.6f','%.5e','%.6f'], header = 'X\tY\tZ\tM200b\tPID')
-
+X,Y,Z,M200b = np.loadtxt("x_y_z_m_sim_selected.txt",skiprows=1,unpack=True)
+L=np.loadtxt("abs_mag_data.txt",unpack=True)
+No_of_L=L.size
 ## Calculating 
 
 
-rho_L=75320/(191)**3.0
+rho_L=No_of_L/(191)**3.0
 print ("rhol=",rho_L)
-m=1e10   ## initial mass
-dm=1e10  ## small increment in mass
+massmin=3e11   ## initial mass
+massmax=np.max(M200b)
+print massmin, massmax
+dm=1e8  ## small increment in mass
 
 i=1e-2
-rho_m=1.48*i  ## initial density of mass from simulation
-while not ((rho_m <= 1.1016*i) and (rho_m >= 1.0584*i)): ## This range is within 2% of rho_L
+rho_m=1.48*i  ## initial density of mass from simulation 
+err = 0.007
+err = 0.007
+while not ((rho_m <= rho_L*(1+err)) and (rho_m >= rho_L*(1-err))): ## This range is within 2% of rho_L
 
 
- range_m = np.where((M200b>= m)&(M200b<=1e15))[0]
+ range_m = np.where((M200b>= massmin)&(M200b<=massmax))[0]
  N=range_m.size
  print N
  rho_m=N/(300)**3.0
- m= dm+m
+ massmin= dm+massmin
 print rho_m
 X1=X[range_m] 
 Y1=Y[range_m]
@@ -32,7 +36,7 @@ Z1=Z[range_m]
 M200b_new= M200b[range_m] 
 print M200b_new
 print X1
-np.savetxt('x1_y1_z1_m_sim.txt',np.transpose([X1,Y1,Z1,M200b_new]),fmt=['%.6f','%.6f','%.6f','%.5e'], header = 'X1\tY1\tZ1\tM200b_new')
+np.savetxt('x1_y1_z1_m_sim_selected.txt',np.transpose([X1,Y1,Z1,M200b_new]),fmt=['%.6f','%.6f','%.6f','%.5e'], header = 'X1\tY1\tZ1\tM200b_new')
 
 
 
@@ -42,4 +46,9 @@ np.savetxt('x1_y1_z1_m_sim.txt',np.transpose([X1,Y1,Z1,M200b_new]),fmt=['%.6f','
 
 
 
+###Check
+print "density of L data=",No_of_L/(191)**3.0
+print "No",No_of_L
+print "density calculated by gitika=",N/(300)**3.0 
 
+print massmin
