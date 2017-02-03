d9f60802737c2237a86e991ad4c0f75775cce40a
Yash <yash.bhargava1992@gmail.com>
2017-02-03 11:17:34 +0530

    selecting one code to save

--- 2_point_correlation_final.py
+++ 2_point_correlation_final.py
@@ -9,7 +9,7 @@ pi = np.pi
 bins = int((75-0.2)/dr)
 hist=np.zeros(bins)
 
-chatter = False 		# Put Chatter True if want more output (like timing info of each segment) on terminal
+chatter = True 			# Put Chatter True if want more output (like timing info of each segment) on terminal
 skip	= 1000			# Number of iteration to skip before printing timing info
 
 
@@ -19,21 +19,20 @@ for item in list_of_mass_sorted_catalogs:
 	
 	start_time = time.time()
 	init_time = time.time()
+	if chatter: print "\n\nWorking with {}".format(item),'\n\n'
 	M,L,X,Y,Z = np.loadtxt (item,unpack=True, skiprows=1)
-	
-	if chatter:print 'Data loaded\nTime taken\t:',time.time()-start_time
+	if chatter: print 'Data loaded\nTime taken\t:',time.time()-start_time
 	start_time = time.time()
 
 	for n in range (X.size):			#Put X.size later
-		hist_temp,edge = pcf.counter_version4(dr,X,Y,Z,n,chatter) 			# Version 3 can also be implemented 
+		hist_temp,edge = pcf.counter(dr,X,Y,Z,n,chatter) 			 
 		hist+= hist_temp
 		if n%skip==0 and chatter:
-			print "Number of points accumulated in 100th bin", hist[99]
 			print "Running loops {} iterations done,\nTime for prev {} run is\t:".format(n,skip),time.time()-start_time
 			start_time = time.time()
 
 	if chatter: print "Completed\nTime for entire operation is\t:",time.time()-init_time
-	file_name = item.split('.')
+	file_name = item.split('.')[0]
 	hist_file_name = 'hist_'+file_name+'.txt'
 	np.savetxt (hist_file_name, np.transpose([hist,edge[:-1]]), fmt=['%.6f','%.6f'])
 
@@ -41,7 +40,7 @@ for item in list_of_mass_sorted_catalogs:
 	DD = hist
 	DD/=(X.size*(X.size-1))/2
 
-	RR = 4*pi*(edge[:-1])**2*dr/(300)**3 				# originally the formula is RR = 4*pi*(r/L)**3*d(ln (r)), d () being differential operator
+	RR = 4*pi*(edge[:-1])**2*dr/(300)**3 				
 
 	Xi = DD/RR-1
 	xi_file_name = 'Xi_'+file_name+'.txt'
