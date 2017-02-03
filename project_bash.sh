ceae188770454c6615ba13e5ea280c61ed60207f
Sujatha Ramakrishnan <sujatha.ramakrishnan.7@gmail.com>
2017-02-03 13:41:57 +0530

    added one missing code

--- project_bash.sh
+++ project_bash.sh
@@ -2,5 +2,6 @@ python saving_important_columns.py
 python extracting_required_data.py
 python density_range_sorting.py
 python abundance_matching.py
+python Generating_Subsets_Of_Mock_Galaxy_Catalog.py
 python 2_point_correlation_final.py
 python final.py
