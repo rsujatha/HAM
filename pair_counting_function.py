ce13f499d8f427cb7b74b9c07aee168dbe2ccfc0
Yash <yash.bhargava1992@gmail.com>
2017-02-03 15:57:46 +0530

    function_update

--- pair_counting_function.py
+++ pair_counting_function.py
@@ -1,7 +1,7 @@
 import numpy as np
 import time
 
-def counter (dr,X,Y,Z,n,chatter):
+def counter (X,Y,Z,n,chatter,bins=None):
 	
 	'''
 	
@@ -10,12 +10,12 @@ def counter (dr,X,Y,Z,n,chatter):
 	
 	Input:
 	---------------------
-	dr					: Thickness of the shell in consideration 
 	X					: X coordinates of all the points
 	Y					: Y coordinates of all the points
 	Z					: Z coordinates of all the points
 	n					: Index number of the point to be computed for
 	chatter				: Boolean variable to determine whether running info is to be printed 
+	bins				: Sequence of bins edges to be used, or none if uniform binning is to be used
 	
 	Output:
 	---------------------
@@ -24,11 +24,10 @@ def counter (dr,X,Y,Z,n,chatter):
 	
 	
 	'''
+	if bins==None: bins= int((75-0.2)/dr)
 	P = np.array([X,Y,Z])
 	p = P[:,n]				## Coordinates of the point in consideration. The (small) cube is made around this point
 	c = P[:,n+1:]			## Shortened array
-	#~ print np.shape(considered_array)
-	#~ print p
 	x,y,z = 0,1,2
 	start_time = time.time()
 	del_x = np.abs(c[x]-p[x])
@@ -42,13 +41,10 @@ def counter (dr,X,Y,Z,n,chatter):
 					((del_y<75)) & 
 					((del_z<75)) ) [0]
 	if n%skip==0 and chatter: print 'time for finding the small cube for {} iter'.format(skip), (time.time()-start_time )*skip
-	#sel_arr = c[:,index]
 	start_time = time.time()
-	#~ dist_array = np.linalg.norm(sel_arr-p[:,None],axis=0)
 	dist_array = np.sqrt( del_x[index]**2+del_y[index]**2+del_z[index]**2)
-	#dist_array = np.sqrt( del_x**2+del_y**2+del_z**2)
 	if n%skip==0 and chatter: print 'time for computing distance for {} iter'.format(skip), (time.time()-start_time )*skip
-	bins = int((75-0.2)/dr)
+	#~ bins = int((75-0.2)/dr)
 	start_time = time.time()
 	hist, edge =  np.histogram(dist_array,bins,range = (0.2,75))
 	if n%skip==0 and chatter: print 'time for making histogram for {} iter'.format(skip), (time.time()-start_time )*skip
