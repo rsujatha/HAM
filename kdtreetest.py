c4d8d3aacef4b756a40e0193c42e2f6d8e467ec5
Yash <yash.bhargava1992@gmail.com>
2017-02-19 21:18:03 +0530

    Removing redundant code segment and beautifying the code

--- kdtreetest.py
+++ kdtreetest.py
@@ -1,63 +1,38 @@
 import numpy as np
 import pair_counting_function as pcf
-from scipy import spatial
+#~ from scipy import spatial as sp
 import time
-import sys
+#~ import sys
 import matplotlib.pyplot as plt
 from matplotlib import rc
+import glob
+
+rc('text',usetex=True)
+rc('font',**{'family':'serif','serif':['Computer Modern']} )
+
 begin=time.time()
 
 pi=np.pi
-M,L,X,Y,Z = np.loadtxt ('mockgalaxySubset_M_less_than_-20.txt',unpack=True, skiprows=1)
-bin_num=20
-pairsN=float(X.size*(X.size-1)/2.)
-DD,bins=pcf.counter_kdtree(X,Y,Z,bin_num)
-DD=DD/pairsN
-rr=bins[1:]**3-bins[:-1]**3
-RR = 4/3.*pi*(rr)/300.**3
-Xi=DD/RR-1
-print Xi.size
-bins_array=(bins[1:]+bins[:-1])/2.
-plt.plot(bins_array,Xi,'-o',label='M<-20')
+
+label = ['M$<-20$','M$<-21$','M$<-22$']
+color = ['r','b','g']
+mock_files = np.sort(glob.glob("mockgalaxySubse*.txt")
+for x in range (xi_files.size):
+	M,L,X,Y,Z = np.loadtxt (mock_files[x],unpack=True, skiprows=1)
+	bin_num=20
+	pairsN=float(X.size*(X.size-1)/2.)
+	DD,bins=pcf.counter_kdtree(X,Y,Z,bin_num)
+	DD=DD/pairsN
+	rr=bins[1:]**3-bins[:-1]**3
+	RR = 4/3.*pi*(rr)/300.**3
+	Xi=DD/RR-1
+	#~ print Xi.size
+	bins_array=(bins[1:]+bins[:-1])/2.
+	plt.plot(bins_array,Xi,'-o',color=color[x],label=label[x])
 plt.xscale('log')
 plt.yscale('log')
-
-
-M,L,X,Y,Z = np.loadtxt ('mockgalaxySubset_M_less_than_-21.txt',unpack=True, skiprows=1)
-bin_num=20
-pairsN=float(X.size*(X.size-1)/2.)
-DD,bins=pcf.counter_kdtree(X,Y,Z,bin_num)
-DD=DD/pairsN
-rr=bins[1:]**3-bins[:-1]**3
-RR = 4/3.*pi*(rr)/300.**3
-Xi=DD/RR-1
-print Xi.size
-bins_array=(bins[1:]+bins[:-1])/2.
-plt.plot(bins_array,Xi,'-o',label='M<-21')
-plt.xscale('log')
-plt.yscale('log')
-
-
-M,L,X,Y,Z = np.loadtxt ('mockgalaxySubset_M_less_than_-22.txt',unpack=True, skiprows=1)
-bin_num=20
-pairsN=float(X.size*(X.size-1)/2.)
-DD,bins=pcf.counter_kdtree(X,Y,Z,bin_num)
-DD=DD/pairsN
-rr=bins[1:]**3-bins[:-1]**3
-RR = 4/3.*pi*(rr)/300.**3
-Xi=DD/RR-1
-print Xi.size
-bins_array=(bins[1:]+bins[:-1])/2.
-plt.plot(bins_array,Xi,'-o',label='M<-22')
-plt.xscale('log')
-plt.yscale('log')
-plt.title('2_point Correlation')
-plt.xlabel('Distance r')
-plt.ylabel(r'$\xi$(r)')
 plt.legend()
 plt.savefig('kdtreeXi.pdf')
 plt.show()
-
-
 
 
