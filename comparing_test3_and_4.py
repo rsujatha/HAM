f0e0ea1fd44da78ce4a6b5ec1a9f5213bd7f0160
Yash <yash.bhargava1992@gmail.com>
2017-02-24 23:06:44 +0530

    Updating_func

--- comparing_test3_and_4.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-from matplotlib import rc
-rc('text',usetex=True)
-rc('font',**{'family':'serif','serif':['Computer Modern']} )
-
-test3 = np.loadtxt ()
